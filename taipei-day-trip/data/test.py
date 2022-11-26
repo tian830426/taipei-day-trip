fd24bb93f38c6ecdb88063452934e0ccbd983442
tian830426 <105027318+tian830426@users.noreply.github.com>
2022-11-26 13:58:33 +0800

    revise app.py

--- taipei-day-trip/data/test.py
+++ taipei-day-trip/data/test.py
@@ -31,8 +31,11 @@ for item in data['result']['results']:
     for i in range(0,fileLen):
         file_format = "https"+item['file'].lower().split('https')[i]          
         if ".jpg" in file_format:    
-            new_file.append(file_format)   
+            new_file.append(file_format) 
+    new_file = str(new_file) .replace("[","").replace("]","").replace(",","").replace("'","")
     item['file'] = new_file
+
+    # print(item['file'])
 
     #增加資料到資料夾
     # mycursor = new.cursor()
@@ -41,10 +44,37 @@ for item in data['result']['results']:
     # mycursor.execute(sql,val)
     # new.commit()
 
+    # 創建第二個資料庫 datas2
+
+    # create table datas2(id int ,
+    # name varchar(255) ,
+    # category varchar(255) ,
+    # description TEXT,
+    # address varchar(255) ,
+    # transport varchar(255) ,
+    # mrt varchar(255) ,
+    # lat float ,
+    # lng  float ,
+    # images TEXT);
+
+
+    #增加資料到資料夾  
+    # mycursor = new.cursor()
+    # sql = "INSERT INTO datas2(id, name, category, description, address, transport, mrt, lat, lng, images) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    # val=(item['_id'], item['name'], item['CAT'].replace(" ",""), item['description'], item['address'], item['direction'], item['MRT'], item['latitude'], item['longitude'], item['file'])
+    # mycursor.execute(sql,val)
+    # new.commit()
+
     # 判斷型別
-    # print(type(item['CAT']))  <class 'str'>
-    # print(type(item['file'])) <class 'list'>
-    # print(type(str(item['file']) ) ) <class 'str'>
+    # print(type(item['CAT']))  
+    # print(type(item['file'])) 
+    # print(type(str(item['file']) ) ) 
+
+
+
+    # mysql报错：1406, "Data too long for column
+    # 參考資料：http://huanyouchen.github.io/2018/05/22/mysql-error-1406-Data-too-long-for-column/
+    # SET @@global.sql_mode= '';
 
     
 
