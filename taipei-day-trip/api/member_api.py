3b05fa6fd68f91864d699eec469b38165e463dc7
tian830426 <105027318+tian830426@users.noreply.github.com>
2023-03-10 15:29:17 +0800

    upload architecture image

--- taipei-day-trip/api/member_api.py
+++ taipei-day-trip/api/member_api.py
@@ -1,17 +1,17 @@
-from flask import *
-import mysql.connector
-import mysql.connector.pooling
-import json
-import jwt
-from api.connector import connection_pool
+# from flask import *
+# import mysql.connector
+# import mysql.connector.pooling
+# import json
+# import jwt
+# from api.connector import connection_pool
 
-member_api = Blueprint("member_api",__name__)
+# member_api = Blueprint("member_api",__name__)
 
 
-@member_api.route("/api/member", methods = ["GET"])
-def api_member():
-    get_cookie_token = request.cookies.get("token")
-    print(get_cookie_token)
-    new_revise_data = request.get_json()
-    print(new_revise_data)
-    return '123'
+# @member_api.route("/api/member", methods = ["GET"])
+# def api_member():
+#     get_cookie_token = request.cookies.get("token")
+#     print(get_cookie_token)
+#     new_revise_data = request.get_json()
+#     print(new_revise_data)
+#     return '123'
